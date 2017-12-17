79bec6a5309eb1c8a620784c42500d781fd4d6c5
Joel Wachsler <joel@wachsler.se>
2017-12-17 18:24:59 +0100

    Forgot to change index

--- project/server/src/index.js
+++ project/server/src/index.js
@@ -1,21 +1,21 @@
 // @ts-check
 // Load environment variables.
-require('dotenv').config()
+require("dotenv").config();
 
-import http from 'http'
-import socketIO from 'socket.io'
-import connectionHandler from './net/connection_handler'
+import http from "http";
+import socketIO from "socket.io";
+import connectionHandler from "./net/connection_handler";
 
 // Init socket.io
-const app = http.createServer()
-const io = socketIO(app)
+const app = http.createServer();
+const io = socketIO(app);
 
 // Init socket connection handler
-connectionHandler(io)
+connectionHandler(io);
 
 // Start to listen
 const listener = app.listen(process.env.PORT, () => {
-	console.log(`Listening to port ${listener.address().port}`)
-})
+  console.log(`Listening to port ${listener.address().port}`);
+});
 
-export default app
+export default app;
