829b3b86771e805637306c43678683318c785408
Joel Wachsler <joel@wachsler.se>
2018-01-03 17:48:31 +0100

    Compiled js removed

--- project/server/dist/common/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.SIZE = void 0; // @ts-check
-
-// Snake block size
-const SIZE = 25;exports.SIZE = SIZE;
-//# sourceMappingURL=constants.js.map
